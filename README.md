eac2fbba9916bfe7946b9b71d9485e5ee288376d
Knifa <knifa@users.noreply.github.com>
2024-12-02 12:00:51 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# led-matrix-zmq-py
+
+A Python library for interacting with [led-matrix-zmq-server](https://github.com/knifa/led-matrix-zmq-server).
---
+++ led_matrix_zmq/__init__.py
@@ -0,0 +1,3 @@
+from .control import LmzControl as LmzControl
+from .control import LmzControlAsync as LmzControlAsync
+from .control import LmzMessageError as LmzMessageError
